c1cca4df4ff6ea6502417c11f2f3a00aede3fb30
MartinPetrakiev <martomsl@gmail.com>
2021-08-05 22:51:54 +0300

    remove comments

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 global.__basedir = __dirname;
 require('dotenv').config()
 const dbConnector = require('./config/db');
-// const mongoose = require('mongoose');
 const apiRouter = require('./router');
 const cors = require('cors');
-// const config = require('./config/config');
 const { errorHandler } = require('./utils');
 
 dbConnector()
